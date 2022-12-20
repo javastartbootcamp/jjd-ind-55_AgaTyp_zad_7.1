1660416f4d818b5b00f61699a610d8ec6fc85b83
Agnieszka Typiak <agnieszka.typiak@mailbox.org>
2022-12-20 14:37:49 +0100

    Rozwiazanie

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -4,6 +4,30 @@ public class PalindromeService {
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
     public boolean isPalindrome(int[] array) {
-        return false;
+
+        boolean isPal = true;
+
+        if (array == null) {
+            isPal = false;
+        } else if (array.length == 0) {
+            isPal = false;
+        } else {
+            int[] revArray = new int[array.length];
+            int j = array.length;
+
+            for (int k : array) {
+                revArray[j - 1] = k;
+                j = j - 1;
+            }
+
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != revArray[i]) {
+                    isPal = false;
+                    break;
+                }
+            }
+        }
+
+        return isPal;
     }
 }
